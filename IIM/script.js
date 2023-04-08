f0603b36fa30bb0c116b99c08d2864e292dd93d6
gopaal24 <gopaal.akkineni@gmail.com>
2023-04-08 12:58:58 +0530

    Added IIM file

---
+++ IIM/script.js
@@ -0,0 +1,53 @@
+const canvas = document.querySelector('.canvas')
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+
+const context = canvas.getContext('2d')
+const frameCount = 10;
+
+const CurrentFrame = (index) => `./bulb-anim/1 (${(index + 1).toString()}).png`;
+const images = []
+const bulb = {frame:0}
+
+for(let i=0;i<frameCount;i++){  
+    const img = new Image();
+    img.src = CurrentFrame(i);
+    images.push(img);
+}
+
+gsap.to(bulb,{
+    frame: frameCount - 1,
+    snap: "frame",
+    ease: "none",
+    scrollTrigger: {
+        scrub: true,
+        pin: "canvas",
+        end: "100%",
+    },
+    onUpdate: render,
+})
+
+images[0].onload = render;
+
+let width_ = images[0].naturalWidth/2;
+let x = 0;
+
+
+function render(){
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+    console.log("called")
+    context.clearRect(0, 0, canvas.width, canvas.height)
+    context.drawImage(images[bulb.frame], -x, 0 , images[bulb.frame].naturalWidth, canvas.height)
+}
+
+window.addEventListener("resize", () => {
+    console.log("resize")
+    render()    
+    if((canvas.width/2)!= width_){
+        x = width_ - canvas.width/2;
+        console.log(x)
+    }
+    
+  })
