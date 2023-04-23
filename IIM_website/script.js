cf893aeb96c7b124a0ab4cb35c5ef0c24f6bb3b2
gopaal24 <gopaal.akkineni@gmail.com>
2023-04-23 09:28:59 +0530

    update

--- IIM_website/script.js
+++ IIM_website/script.js
@@ -1,57 +1,3 @@
-//Contact
-const contact_btn = document.querySelector(".contact-btn")
-const close_btn = document.querySelector(".close-btn")
-const popup = document.querySelector(".popup")
-const main_popup = document.querySelector(".main-popup")
-
-const contact_btn1 = document.querySelector(".contact-btn1")
-const close_btn1 = document.querySelector(".close-btn1")
-const popup1 = document.querySelector(".popup1")
-const main_popup1 = document.querySelector(".main-popup1")
-
-contact_btn.addEventListener("click", ()=>{
-    popup.style.display = "flex"
-    main_popup.style.cssText = "animation: slide-in .5s ease;animation-fill-mode: forwards;"
-})
-
-close_btn.addEventListener("click", ()=>{
-    main_popup.style.cssText = "animation: slide-out .5s ease;animation-fill-mode: forwards;"
-    setTimeout(()=>{
-        popup.style.display = 'none'
-    }, 600)
-})
-
-window.addEventListener('click', (e)=>{
-    if(e.target == document.querySelector(".popup-overlay")){
-        main_popup.style.cssText = "animation: slide-out .5s ease;animation-fill-mode: forwards;"
-    setTimeout(()=>{
-        popup.style.display = 'none'
-    }, 600)
-    }
-})
-
-
-contact_btn1.addEventListener("click", ()=>{
-    popup1.style.display = "flex"
-    main_popup1.style.cssText = "animation: slide-in .5s ease;animation-fill-mode: forwards;"
-})
-
-close_btn1.addEventListener("click", ()=>{
-    main_popup1.style.cssText = "animation: slide-out .5s ease;animation-fill-mode: forwards;"
-    setTimeout(()=>{
-        popup1.style.display = 'none'
-    }, 600)
-})
-
-window.addEventListener('click', (e)=>{
-    if(e.target == main_popup1){
-        main_popup1.style.cssText = "animation: slide-out .5s ease;animation-fill-mode: forwards;"
-    setTimeout(()=>{
-        popup1.style.display = 'none'
-    }, 600)
-    }
-})
-
 
 //Navbar Scroll
 const menu = document.querySelector("#checkbox_toggle")
@@ -134,4 +80,23 @@ left.addEventListener('click', ()=>{
     if(index < 0) index=3;
     matter.innerHTML = messages[index];
     image.style.backgroundImage = "url('images/"+images[index]+".jpg')";
+})
+
+
+//Announce
+const announce_btn = document.querySelector('.announcement button')
+const announce_box = document.querySelector('.announce')
+let announce_toggle = false
+
+announce_btn.addEventListener('click', ()=>{
+    if(!announce_toggle){
+        announce_box.style.display = 'block'
+        announce_box.style.cssText = "animation: slide-in .5s ease;animation-fill-mode: forwards;"
+        announce_toggle = !announce_toggle
+    }
+    else{
+        announce_box.style.display = 'none'
+        announce_box.style.cssText = "animation: slide-out .5s ease;animation-fill-mode: forwards;"
+        announce_toggle = !announce_toggle
+    }
 })
